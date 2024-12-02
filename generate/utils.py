61cf0d4e690895f7cd555d9ab824ceac7a6e62bf
PicoHBK <picohbk322@gmail.com>
2024-12-02 14:19:55 -0300

    fix res

--- generate/utils.py
+++ generate/utils.py
@@ -14,14 +14,14 @@ def modificar_json(file_path, new_prompt, new_hr_prompt, neg_prompt, image_type)
     modified_data["negative_prompt"] = f"{modified_data['negative_prompt']},{neg_prompt}"
     
     if image_type == "Portrait":
-        modified_data["width"] = 500
-        modified_data["height"] = 500
+        modified_data["width"] = 720
+        modified_data["height"] = 560
     elif image_type == "Full Body":
         modified_data["width"] = 448
         modified_data["height"] = 888
     elif image_type == "Cowboy Shot":
-        modified_data["width"] = 500
-        modified_data["height"] = 700
+        modified_data["width"] = 510
+        modified_data["height"] = 710
     
     # Retornar el diccionario modificado
     return modified_data
