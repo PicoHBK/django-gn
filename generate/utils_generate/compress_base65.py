52b23a61c85a30779a7b19701f7cb89b646fa417
PicoHBK <picohbk322@gmail.com>
2025-08-18 21:34:43 -0300

    fox

--- generate/utils_generate/compress_base65.py
+++ generate/utils_generate/compress_base65.py
@@ -4,36 +4,27 @@ from io import BytesIO
 
 def compress_images_base64(image_list: list) -> list:
     compressed_images = []
-
     # Mostrar el tamaño de la lista original en MB
     original_size_mb = sum(len(image) for image in image_list) / (1024 * 1024)
     print(f"Tamaño original de las imágenes: {original_size_mb:.2f} MB")
-
+    
     for image_data in image_list:
         # Convertir la cadena base64 a bytes
         image_bytes = base64.b64decode(image_data)
-
         # Usar PIL para abrir la imagen
         image = Image.open(BytesIO(image_bytes))
-
         # Crear un buffer para guardar la imagen en formato WebP
         buf = BytesIO()
-
-        # Guardar la imagen como WebP con compresión (puedes ajustar la calidad aquí)
-        quality = 95 # La calidad de la imagen WebP (0-100)
-        image.save(buf, format="WebP", quality=quality)
-
+        # Guardar la imagen como WebP sin pérdida de calidad
+        image.save(buf, format="WebP", lossless=True)
         # Obtener los datos de la imagen en WebP
         webp_image_data = buf.getvalue()
-
         # Convertir los datos a base64
         compressed_base64 = base64.b64encode(webp_image_data).decode('utf-8')
-
         # Agregar la imagen comprimida a la lista
         compressed_images.append(compressed_base64)
-
+    
     # Mostrar el tamaño de la lista comprimida en MB
     compressed_size_mb = sum(len(image) for image in compressed_images) / (1024 * 1024)
-    print(f"Tamaño comprimido de las imágenes: {compressed_size_mb:.2f} MB")
-
+    print(f"Tamaño después de optimización (sin pérdida): {compressed_size_mb:.2f} MB")
     return compressed_images  # Retorna la lista de imágenes en base64
